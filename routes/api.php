677d2718af4fe40aae99f5212942cb09122254a3
Amir <abouei.me@gmail.com>
2024-05-23 11:08:02 +0330

    feat: remove extra api

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,9 @@
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api')->group(function (){
 
-    Route::prefix('backoffice')->group(function (){
+Route::prefix('backoffice')->group(function (){
 
         Route::get('orders', [OrderController::class, 'index']);
 
     });
-
-});
